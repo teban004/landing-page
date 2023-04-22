a62ca700bd06b562ce83693c500de39c085c5f7d
Mr. Robot <mrrobot@bunker.cyberstb.net>
2023-04-22 18:40:04 -0400

    added grid to the links section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { FaGoogle, FaFacebook, FaTwitter } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <div className="links-section">
-        {/* TODO: Add links grid */}
+        <div className="links-grid">
+          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <div className="link-button">
+              <FaGoogle size={30} />
+              <span>Google</span>
+            </div>
+          </a>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <div className="link-button">
+              <FaFacebook size={30} />
+              <span>Facebook</span>
+            </div>
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <div className="link-button">
+              <FaTwitter size={30} />
+              <span>Twitter</span>
+            </div>
+          </a>
+          {/* Add more links as needed */}
+        </div>
       </div>
       <div className="components-section">
         {/* TODO: Add components */}
